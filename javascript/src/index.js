ac92f58d6c71e568a5b7833dba56aac3e61ede3a
skuethe <crazycapivara@gmail.com>
2018-11-30 13:48:39 +0100

    Make the hexagon js example work

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,6 +1,9 @@
 import R2DeckGL from "./r2deckgl";
 import {x} from "./data/x-hexagon-layer";
 
+global.HTMLWidgets = {};
+
 var el = document.getElementById("widget");
 var r2DeckGL = new R2DeckGL(el);
-el.innerHTML = r2DeckGL.renderValue(x);
+// el.innerHTML = r2DeckGL.renderValue(x);
+r2DeckGL.renderValue(x);
--- javascript/src/r2deckgl.js
+++ javascript/src/r2deckgl.js
@@ -1,18 +1,54 @@
 var deck = global.deck;
+var deckglWidget = global.deckglWidget = {};
 
-var logVersions = function() {
-  console.log("deck.gl version: " + deck.version);
-  if (typeof mapboxgl !== "undefined") {
-    console.log("mapbox-gl version: " + mapboxgl.version);
-  }
+deckglWidget.colorToRGBArray = function(color) {
+  color = color.substring(1); // remove '#'
+  return [
+    parseInt(color.substring(0, 2), 16),
+    parseInt(color.substring(2, 4), 16),
+    parseInt(color.substring(4), 16)
+    ];
 };
 
 export default function(el) {
   console.log("r2deckgl", el);
 
+  deckglWidget.element = el;
+  var tooltipElement = deckglWidget.tooltipElement = document.createElement("div");
+  tooltipElement.id = "tooltip";
+  el.appendChild(tooltipElement);
+
+  var deckgl = null;
+
   this.renderValue = function(x) {
     logVersions();
     console.log(x);
+
+    var properties = {
+      mapboxApiAccessToken: x.mapboxApiAccessToken || "",
+      mapStyle: x.mapStyle || "",
+      container: el.id,
+      initialViewState: x.initialViewState || newInitialViewState(x),
+      views: x.views || new MapView(),
+      layers: []
+    };
+    properties = Object.assign(properties, x.properties);
+    console.log(properties);
+
+    deckgl = deckglWidget.deckgl = new deck.DeckGL(properties);
+
+    deckglWidget.layers = x.layers.map(function(item) {
+      if (item.properties.dataframeToD3) {
+        item.data = HTMLWidgets.dataframeToD3(item.data);
+      }
+
+      item.properties.data = item.data;
+      console.log(item);
+      return newLayer(item.className, item.properties);
+    });
+
+    deckgl.setProps({ layers: deckglWidget.layers });
+
     return "r2deckgl";
   };
 
@@ -20,3 +56,51 @@ export default function(el) {
 
   };
 }
+
+// -----
+var logVersions = function() {
+  console.log("deck.gl version: " + deck.version);
+  if (typeof mapboxgl !== "undefined") {
+    console.log("mapbox-gl version: " + mapboxgl.version);
+  }
+};
+
+var newInitialViewState = function(x) {
+  return {
+    longitude: x.longitude,
+    latitude: x.latitude,
+    zoom: x.zoom,
+    pitch: x.pitch,
+    bearing: x.bearing
+  };
+};
+
+var newLayer = function(className, properties) {
+  // Pass data back to R in 'shinyMode'
+  if (HTMLWidgets.shinyMode) {
+    properties.onClick = function(info) {
+      var data = { lng: info.lngLat[0], lat: info.lngLat[1], object: info.object };
+      Shiny.onInputChange(deckglWidget.element.id + "_onclick", data);
+    };
+  }
+
+  if (properties.getTooltip) {
+    properties.onHover = function({x, y, object}) {
+      var tooltipElement = deckglWidget.tooltipElement;
+      if (!object) {
+        tooltipElement.innerHTML = "";
+        return;
+      }
+
+      var text = properties.getTooltip(object);
+      if (!properties.fixedTooltip) {
+        tooltipElement.style.top = y + "px";
+        tooltipElement.style.left = x + "px";
+      }
+
+      tooltipElement.innerHTML = text;
+    };
+  }
+
+  return new deck[className](properties);
+};
